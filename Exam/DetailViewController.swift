1237da9a0a890254a6dd73ddb99826b36f87ba55
shashikiran330 <shashikiran330@gmail.com>
2020-03-07 18:34:51 +0530

    Add files via upload

---
+++ Exam/DetailViewController.swift
@@ -0,0 +1,23 @@
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    var film: Film!
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var lblDirector: UILabel!
+    @IBOutlet weak var lblPro: UILabel!
+    @IBOutlet weak var lblDate: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        overrideUserInterfaceStyle = .light
+        navigationController?.navigationBar.barTintColor = UIColor.blue
+        self.title = film.title
+        
+        self.lblDate.text = film.releaseDate
+        self.lblName.text = film.title
+        self.lblPro.text = film.producer
+        self.lblDirector.text = film.director
+    }
+}
